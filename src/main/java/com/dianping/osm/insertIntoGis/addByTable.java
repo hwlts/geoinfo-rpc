c347a4a7262a75a0094cb7a4c27cdbd294442dff
hwlts <hwlts@qq.com>
2013-08-27 16:39:15 +0800

    addByTable

--- src/main/java/com/dianping/osm/insertIntoGis/addByTable.java
+++ src/main/java/com/dianping/osm/insertIntoGis/addByTable.java
@@ -1,6 +1,46 @@
 package com.dianping.osm.insertIntoGis;
 
+import com.dianping.osm.sql.MySQLCon;
+import com.dianping.osm.sql.PostgreSQLCon;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
 public class addByTable
 {
+    private Map<String,String> CityNameToPinYin = new HashMap<String,String>();
+    {
+        CityNameToPinYin.put("上海","shanghai");
+        CityNameToPinYin.put("","");
+    }
 
+    private Map<String,String> CityNameToIndex = new HashMap<String,String>();
+    {
+        CityNameToIndex.put("","");
+    }
+
+    public void addCity(String city)
+            throws SQLException
+    {
+        PostgreSQLCon postgreSQLCon = new PostgreSQLCon();
+        Connection postgresqlCon = postgreSQLCon.getConnection();
+        Statement selectSt = postgresqlCon.createStatement();
+
+        MySQLCon mySQLCon = new MySQLCon();
+        Connection mysqlcon = mySQLCon.getConnection();
+        if (mysqlcon != null)
+        {
+            String transverse = "SELECT * FROM ";
+            Statement mysqlSt = mysqlcon.createStatement();
+            ResultSet mysqlRs = mysqlSt.executeQuery(transverse);
+            while (mysqlRs.next())
+            {
+                String s;
+            }
+        }
+    }
 }
